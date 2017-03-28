5802505522a04ae85ea8b3e7a25cfda3299a3970
Alexey Kolechkin <alexey.kolechkin@gmail.com>
2017-03-28 10:27:25 -0700

    Controls
    Still a WIP, but works somewhat
    Going to completely redo them later

--- Assets/Scenes/Hayden_Demo.gd
+++ Assets/Scenes/Hayden_Demo.gd
@@ -5,12 +5,39 @@ extends Node
 # var b = "textvar"
 
 func _ready():
-	self.set_process(true)
+	#self.set_process(true)
+	self.set_process_input(true)
+
+func _input(event):
+	if(event.type == InputEvent.KEY):
+		var curPos = self.get_pos()
+		
+		if(event.scancode == KEY_D):
+			curPos.x += 10
+			self.set_pos(curPos)
+			#self.get_tree().set_input_as_handled()
+	if(event.type == InputEvent.KEY):
+		var curPos = self.get_pos()
+		
+		if(event.scancode == KEY_A):
+			curPos.x -=10
+			self.set_pos(curPos)
+			#self.get_tree().set_input_as_handled()
+	if(event.type == InputEvent.KEY):
+		var curPos = self.get_pos()
+		
+		if(event.scancode == KEY_W):
+			curPos.y -=10
+			self.set_pos(curPos)
+			#self.get_tree().set_input_as_handled()
+	if(event.type == InputEvent.KEY):
+		var curPos = self.get_pos()
+		
+		if(event.scancode == KEY_S):
+			curPos.y +=10
+			self.set_pos(curPos)
+			#self.get_tree().set_input_as_handled()
 
 func _process(delta):
-	var curPos = self.getPos()
-	curPos.x += 100 * delta
-	
-	if(curPos.x > self.get_viewport_rect().size.width + self.get_item_rect().size.width/2):
-		curPos.x = -self.get_item_rect().size.width/2
-	self.set_pos(curPos)
+	if(Input.is_key_pressed(KEY_ESCAPE)):
+		self.get_tree().quit()
